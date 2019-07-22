d1935827bcbffd547e3c83e8d79657042eb179ff
Gernot Stangl <gs@viewar.com>
2019-07-22 10:30:19 +0200

    call getDirection with 3D coordinates

--- src/app.js
+++ src/app.js
@@ -8,16 +8,18 @@ import React, { Fragment }  from 'react';
 import template from './app.jsx';
 import { getDirection } from './utils/directions';
 
+const REFRESH = 25 // ms
+
 let updateInterval = 0;
 const direction = ({setDirPoint}) => async () => {
     updateInterval = setInterval(async () => {
-        setDirPoint(await getDirection());
-    }, 25);
+        setDirPoint(await getDirection(0, 0, 0));
+    }, REFRESH);
 }
 
 export default compose(
     withState('direction', 'setDirection', false),
-    withState('dirPoint', 'setDirPoint', { x: 0, y:0 }),
+    withState('dirPoint', 'setDirPoint', { x: 0, y:0, rot: 0, display: 'block' }),
     withHandlers({ 
         direction,
     }),
--- src/utils/directions.js
+++ src/utils/directions.js
@@ -1,41 +1,44 @@
-// directions util: input a screen coordinate outside of the screen, 
-// get the nearest point at the edge of the screen
+// directions util: input a point in space 
+// if this point is not visible on screen:
+// return a screen coordinate in percent of screen dimension at the edge of the screen
+// and the rotation of the axis from screen center to the screen coordinate
+// and a css display attribute
+
 import viewarApi from 'viewar-api';
 
-const COORD = 1.0;
-
-export const getDirection = async () => {
-    const screen = await viewarApi.coreInterface.call('getScreenSpaceCoordinates', JSON.stringify([ { x: COORD, y: COORD, z: COORD }]));
+export const getDirection = async ( xCoord, yCoord, zCoord ) => {
+    const screen = await viewarApi.coreInterface.call('getScreenSpaceCoordinates', JSON.stringify([ { x: xCoord, y: yCoord, z: zCoord }]));
     if ((!screen) || screen[0].x === 'undefined' || screen[0].x === 'null' || screen[0].y === 'undefined' || screen[0].y === 'null') {
         return { x: 0, y: 0, rot: 0, display: 'none'};
     }
+    // screen coords range from 0 to 1
     if (screen[0].x > 0 && screen[0].x < 1 && screen[0].y > 0 && screen[0].y < 1) {
         return { x: 0, y: 0, rot: 0, display: 'none'};
     }
 
-    const OFFSET = 0.5; // distance from screen edge to center of screen
+    const percent = 100;
+    const offset = percent/2; // distance from screen edge to center of screen
     let x0, y0, xScale, yScale, x1, y1, rad;
     // calculate distance of screen point to center
-    x0 = screen[0].x - OFFSET;
-    y0 = screen[0].y - OFFSET;
-    rad = 0; // remove
+    x0 = screen[0].x * percent - offset;
+    y0 = screen[0].y * percent - offset;
     if (x0 && y0) {
         // scale so point x is on screen edge
-        xScale = OFFSET/Math.abs(x0);
+        xScale = offset/Math.abs(x0);
         y1 = y0 * xScale;
         x1 = x0 * xScale;
-        if (Math.abs(y1) > OFFSET) {
+        if (Math.abs(y1) > offset) {
             // if y exceeds the screen edge scale down again
-            yScale = OFFSET/Math.abs(y1);
-            y1 > OFFSET ? y1 = OFFSET : y1 = OFFSET * (-1);
+            yScale = offset/Math.abs(y1);
+            y1 > offset ? y1 = offset : y1 = offset * (-1);
             x1 = x1 * yScale;
         }
-    } 
-    if (x0 === 0 && y0 >= OFFSET) { x1 = 0; y1 = OFFSET; }
-    if (x0 === 0 && y0 < OFFSET) { x1 = 0; y1 = OFFSET * (-1); }
-    if (y0 === 0 && x0 >= OFFSET) { x1 = OFFSET; y1 = 0; }
-    if (y0 === 0 && x0 < OFFSET) { x1 = OFFSET * (-1); y1 = 0; }
+    }
+    if (x0 === 0 && y0 >= offset) { x1 = 0; y1 = offset; }
+    if (x0 === 0 && y0 < offset) { x1 = 0; y1 = offset * (-1); }
+    if (y0 === 0 && x0 >= offset) { x1 = offset; y1 = 0; }
+    if (y0 === 0 && x0 < offset) { x1 = offset * (-1); y1 = 0; }
     rad = Math.atan2(y1, x1);
-    return { x: (x1 + OFFSET) * 95 + '%', y: (y1 + OFFSET) * 95 + '%', rot: rad + 'rad', display: 'block'};
+    return { x: (x1 + offset) + '%', y: (y1 + offset) + '%', rot: rad + 'rad', display: 'block'};
 
 }
