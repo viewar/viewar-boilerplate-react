d027d08b541d77779cdc8bd23333352d2bc5ef7b
viewar_sf <dev@project23.org>
2019-09-22 01:38:31 +0200

    feat(@viewar/webpack): use pckg from github

--- src/app.js
+++ src/app.js
@@ -1,5 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
+import viewarApi from 'viewar-api';
 
-const App = () => <h1>ViewAR SDK</h1>;
+
+class App extends Component {
+  async componentDidMount() {
+    // load demo 3d model
+    const sheepModel = await viewarApi.modelManager.fetchModelFromRepository('20');
+    // renders model into 3D layer of viewar-core
+    for (let x = 0; x < 20; ++x) {
+      await viewarApi.sceneManager.insertModel(sheepModel, {
+        pose: {
+          position: {
+            y: 0,
+            x: Math.random() * 4000 - 2000,
+            z: Math.random() * 4000 - 2000,
+          },
+        },
+      });
+    }
+  }
+
+  render() {
+    return (
+      <h1>ViewAR SDK</h1>);
+  }
+}
+
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,48 +1,20 @@
-import React from 'react';
+import React    from 'react';
 import ReactDOM from 'react-dom';
 import viewarApi from 'viewar-api';
 
-import App from './app';
-
+import App from './App';
 import './remote-console';
-import './index.scss';
+// import style from './index.scss';
 
 (async function() {
-  window.api = await viewarApi.init();
-
+  window.api        = await viewarApi.init();
   const rootElement = document.getElementById('app');
 
-  const render = (Component) => {
-    ReactDOM.render(<Component />, rootElement);
-  };
-
-  render(App);
+  ReactDOM.render(<App />, rootElement);
 
   if (module.hot) {
     module.hot.accept(App, () => {
-      render(App);
+      ReactDOM.hydrate(App);
     });
   }
-
-  const sheepModel = await viewarApi.modelManager.fetchModelFromRepository('20');
-
-  for (let x = 0; x < 20; ++x) {
-    await viewarApi.sceneManager.insertModel(sheepModel, {
-      pose: {
-        position: {
-          x: Math.random() * 4000 - 2000,
-          y: 0,
-          z: Math.random() * 4000 - 2000,
-        },
-      },
-    });
-  }
-
-  console.log('log', {
-    a: 1234,
-    b: '5678',
-  });
-  console.info('info');
-  console.warn('warn');
-  console.error('error');
 })();
